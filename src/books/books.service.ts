5c9d09ea7337c1cf56c728ac23da67783b63bfc8
Vivian Sze <vivian@upstate.agency>
2020-02-19 23:31:30 -0800

    create book project

---
+++ src/books/books.service.ts
@@ -0,0 +1,39 @@
+
+  import { Injectable, HttpException } from '@nestjs/common';
+  import { BOOKS } from '../mocks/books.mock';
+
+  @Injectable()
+  export class BooksService {
+    books = BOOKS;
+    getBooks(): Promise<any> {
+      return new Promise(resolve => {
+          resolve(this.books);
+      });
+    }
+    getBook(bookID): Promise<any> {
+      const id = Number(bookID);
+      return new Promise(resolve => {
+        const book = this.books.find(book => book.id === id);
+        if (!book) {
+          throw new HttpException('Book does not exist!', 404);
+        }
+        resolve(book);
+      });
+    }
+    addBook(book): Promise<any> {
+      return new Promise(resolve => {
+        this.books.push(book);
+        resolve(this.books);
+      });
+    }
+    deleteBook(bookID): Promise<any> {
+      return new Promise(resolve => {
+        const newList = this.books.filter((book) => {
+          if(book.id !== bookID) {
+            return book
+          }
+        })
+        resolve(newList);
+      });
+    }
+  }
---
+++ src/mocks/books.mock.ts
@@ -0,0 +1,8 @@
+export const BOOKS = [
+  { id: 1, title: 'Ella Enchanted', description: "This is the description for the first book", author: 'Olususi Oluyemi' },
+  { id: 2, title: 'Old Man By The Sea', description: "This is the description for the second book", author: 'John Barry' },
+  { id: 3, title: 'Catcher In The Rye', description: "This is the description for the third book", author: 'Clement Wilfred' },
+  { id: 4, title: 'Romeo and Juliet', description: "This is the description for the fourth book", author: 'Christian nwamba' },
+  { id: 5, title: 'Hamlet', description: "This is the description for the fifth book", author: 'Chris anderson' },
+  { id: 6, title: 'Othello', description: "This is the description for the sixth book", author: 'Olususi Oluyemi' },
+];
